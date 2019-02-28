f57c49838a8ca95a3e05a0029a3b498267486ed0
Steve Workman <steve.workman@maersk.com>
2019-02-28 15:16:56 +0000

    Minimal preset to be used

---
+++ generator.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+module.exports = (api, options, rootOptions) => {
+
+  api.exitLog(
+    "The location path that is reported for this function looks very odd"
+  );
+};
